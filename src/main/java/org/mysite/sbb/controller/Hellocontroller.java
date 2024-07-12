48b53232eae7b4bf8da20cff760801bff2d72ea3
Sando0617 <kimhuuu@naver.com>
2024-07-12 10:36:18 +0900

    뷰템플릿 헬로월드

--- src/main/java/org/mysite/sbb/controller/Hellocontroller.java
+++ src/main/java/org/mysite/sbb/controller/Hellocontroller.java
@@ -2,14 +2,12 @@ package org.mysite.sbb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Hellocontroller {
 	
 	@GetMapping("/")
-	@ResponseBody
 	public String hello() {
-		return "Hello World";
+		return "hello_controller";
 	}
 }
